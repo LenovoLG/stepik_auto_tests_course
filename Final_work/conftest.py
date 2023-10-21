3fa87a9a5230c707825f05b532a518cab664da9d
LenovoLG <147095300+LenovoLG@users.noreply.github.com>
2023-10-22 00:50:21 +0600

    Update conftest.py
    conftes

--- Final_work/conftest.py
+++ Final_work/conftest.py
@@ -1,1 +1,8 @@
-
+# conftest.py
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="ru",
+        help="Specify the language for the test run",
+    )
